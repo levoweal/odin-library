804e99812cc7397f3af4ac1a24adfc463e93df53
levoweal <69198520+levoweal@users.noreply.github.com>
2026-04-16 20:00:36 +0400

    Add basic functionality and remove button

--- script.js
+++ script.js
@@ -27,20 +27,24 @@ function cellFunction(book, frag) {
     length.textContent =`amount of pages: ${book.length}`;
     bookCell.appendChild(length);
 
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', () => {
+        const kill = library.findIndex(item => item.id === book.id);
+        if (kill !== -1) {library.splice(kill, 1)};
+        gridFunction();
+    })
+    bookCell.appendChild(removeBtn);
+
     frag.appendChild(bookCell);
 }
-
-
-addBook('book about stuff', 'dude from boston', 100);
-addBook('history of halley', 'fred from halley', 22081);
-addBook('recollection', 'jeremy', 120);
-addBook('test', 'test', 'test');
-
 
 const content = document.querySelector('.content');
 const sidebar = document.querySelector('.sidebar');
 
 function gridFunction() {
+    const divs = document.querySelectorAll('.content div');
+    divs.forEach(item => {item.remove()});
     const frag = document.createDocumentFragment();
     library.forEach(item => cellFunction(item, frag));
     content.appendChild(frag);
@@ -78,6 +82,22 @@ addNewBtn.addEventListener('click', () => {
     form.appendChild(labelFactory('author', 'text', 'Author: '))
     form.appendChild(labelFactory('pages', 'number', 'Amount of pages: '))
 
-    sidebar.appendChild(frag);
-})
+    const submit = document.createElement('button');
+    submit.type = 'submit';
+    submit.textContent = 'Confirm';
+    submit.className = 'submit';
+    form.appendChild(submit);
 
+    sidebar.appendChild(frag);
+
+    addNewBtn.style.visibility = 'hidden';
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const fd = new FormData(form);
+        addBook(fd.get('title'), fd.get('author'), fd.get('pages'));
+        gridFunction();
+        form.remove();
+        addNewBtn.style.visibility = 'visible';
+    })
+})
